9070849ecc183ced4200992aa5c1c3383e73d1b5
T_Mori <morit0301@gmail.com>
2024-05-30 20:01:06 +0900

    ✨ Routing,Navigation,Login

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import CreatePost from "./components/CreatePost";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import NavBar from "./components/Navbar";
+import { useState } from "react";
+
+function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/createpost" element={<CreatePost />}></Route>
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faFilePen,
+  faArrowRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <Link to="/">
+        <FontAwesomeIcon icon={faHouse} />
+        ホーム
+      </Link>
+      <Link to="/createpost">
+        <FontAwesomeIcon icon={faFilePen} />
+        記事投稿
+      </Link>
+      <Link to="/login">
+        <FontAwesomeIcon icon={faArrowRightFromBracket} />
+        ログイン
+      </Link>
+    </nav>
+  );
+};
+
+export default NavBar;
